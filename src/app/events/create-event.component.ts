b2fbf312d07a0e597bb02a9f4d49efc6f952d662
CyrusWarner <cyruswarner27@gmail.com>
2021-10-04 14:40:58 -0500

    Added functionality for clicking cancel and being returned to events list route.

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-
+import { Router } from "@angular/router";
 @Component({
     template: `
     <h1>New Event</h1>
@@ -8,11 +8,16 @@ import { Component } from "@angular/core";
     <br/>
     <br/>
     <button type="submit" class="btn btn-primary">Save</button>
-    <button type="button" class="btn btn-default">Cancel</button>
+    <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
     </div>
     `
 })
 
 export class CreateEventComponent {
+    constructor(private router: Router){
 
+    }
+    cancel(){
+        this.router.navigate(['/events'])
+    }
 }
