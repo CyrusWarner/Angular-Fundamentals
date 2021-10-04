ca6b877c30d916262ecd6f92e206188fc3346087
CyrusWarner <cyruswarner27@gmail.com>
2021-10-04 13:19:34 -0500

    Created event details component and added method for getting a single event in event services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 // services
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
@@ -17,7 +18,8 @@ import { ToastrService } from './common/toastr.service';
     EventsAppComponent,
     EventsListComponent,
     EventsThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent
   ],
   providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
