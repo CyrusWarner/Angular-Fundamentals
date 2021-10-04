9dc01075aa3e70263036c1a3245b59ccf0aae68f
CyrusWarner <cyruswarner27@gmail.com>
2021-10-04 12:21:39 -0500

    Replaced ngClass with ngStyle.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,7 +9,7 @@ import { Component, Input } from '@angular/core';
   template: ` <div class="well hoverwell thumbnail">
     <h2>{{ event?.name }}</h2>
     <div>Date: {{ event?.date }}</div>
-    <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+    <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
       Time: {{ event?.time }}
       <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
       <span *ngSwitchCase="'10:00 am'">(late Start)</span>
@@ -52,10 +52,10 @@ import { Component, Input } from '@angular/core';
 export class EventsThumbnailComponent {
   @Input() event: any;
 
-  getStartTimeClass() {
+  getStartTimeStyle():any {
     if (this.event && this.event.time === '8:00 am') {
-      return ['green', 'bold'];
+      return {color: '#003300', 'font-weight': 'bold'}
     }
-    return [];
+    return {};
   }
 }
