715e5c91eff12e018380133b2ba5650b7d565849
CyrusWarner <cyruswarner27@gmail.com>
2021-10-04 14:33:27 -0500

    Added route for creating a new event.

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
 import { EventsListComponent } from "./app/events/events-list.component";
+import { CreateEventComponent } from "./app/events/create-event.component";
+
 import { Routes } from "@angular/router";
 
 export const appRoutes:Routes = [
+    {path: 'events/new', component: CreateEventComponent},
     {path: 'events', component: EventsListComponent},
     {path: 'events/:id', component: EventDetailsComponent},
     {path: '', redirectTo: '/events', pathMatch: 'full'}
