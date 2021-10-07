12d11957362867672baae4d80917aae84fabf51f
CyrusWarner <cyruswarner27@gmail.com>
2021-10-07 17:42:51 -0500

    Added form for updating the current user with reactive form.

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -18,4 +18,9 @@ export class AuthService {
         }
         return false;
     }
+
+    updateCurrentUser(firstName:string, lastName:string){
+        this.currentUser.firstName = firstName;
+        this.currentUser.lastName = lastName;
+    }
 }
