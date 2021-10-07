69e6d63959305c43c3971660141cc31e2b0e4b78
CyrusWarner <cyruswarner27@gmail.com>
2021-10-07 15:05:24 -0500

    Added authService for logging a user in.

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { IUser } from "./user.model";
+@Injectable()
+export class LoginUser {
+    currentUser: IUser
+    loginUser(userName:string, password:string){
+
+    }
+}
