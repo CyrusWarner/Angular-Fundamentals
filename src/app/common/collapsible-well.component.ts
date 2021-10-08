f502ac5411deae81d7a45b884afb1bd797736b3c
CyrusWarner <cyruswarner27@gmail.com>
2021-10-08 17:18:51 -0500

    Added fire icon to display if  a sessions has more than 3 voters.

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
-
+// Content projection selects an attribute with the [] <-- square brackets
 @Component({
   selector: 'collapsible-well',
   template: `
     <div (click)="(toggleContent())" class="well pointable">
-      <h4 class="well-title">{{ title }}</h4>
-      <ng-content *ngIf="visible"></ng-content>
+      <h4 >
+        <ng-content select="[well-title]"></ng-content>
+      </h4>
+      <ng-content select="[well-body]" *ngIf="visible"></ng-content>
     </div>
   `,
 })
 export class CollapsibleWellComponent {
-  @Input() title: string;
   visible: boolean = true;
   toggleContent() {
     this.visible = !this.visible;
