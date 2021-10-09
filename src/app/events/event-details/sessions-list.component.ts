e49b27a9699d658a11ebda9c75b0c9ffa7195d38
CyrusWarner <cyruswarner27@gmail.com>
2021-10-09 14:31:26 -0500

    Implemented functionality for sorting by name and by votes.

--- src/app/events/event-details/sessions-list.component.ts
+++ src/app/events/event-details/sessions-list.component.ts
@@ -7,12 +7,14 @@ import { ISession } from '..';
 export class SessionsListComponent implements OnChanges {
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[] = [];
   // ngOnChanges is called everytime one of the input values gets a new value
   // Similar to component did update
   ngOnChanges(){
     if(this.sessions){
       this.filterSessions(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAscending) : this.visibleSessions.sort(sortByVotesDescending)
     }
   }
   // this.sessions.slice(0) creates a duplicate of the sessions array with all the same elements
@@ -26,4 +28,14 @@ export class SessionsListComponent implements OnChanges {
 
     }
   }
+}
+
+function sortByNameAscending(s1: ISession, s2: ISession){
+  if(s1.name > s2.name) return 1
+  else if(s1.name === s2.name) return 0
+  else return -1
+}
+
+function sortByVotesDescending(s1: ISession, s2: ISession){
+  return s2.voters.length - s1.voters.length;
 }
