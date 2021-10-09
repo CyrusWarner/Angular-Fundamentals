041360f8665642821f7b238a9392efe7550c4339
CyrusWarner <cyruswarner27@gmail.com>
2021-10-09 13:34:08 -0500

    Added uppercase pipe to event titles and date pipe to event dates.

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-// Content projection selects an attribute with the [] <-- square brackets
+// multiple content projection selects an attribute with the [] <-- square brackets
 @Component({
   selector: 'collapsible-well',
   template: `
