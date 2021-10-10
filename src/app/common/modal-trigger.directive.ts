1212f78f7d1ef5c6ed81f765873f2be625e1d1fa
CyrusWarner <cyruswarner27@gmail.com>
2021-10-10 15:47:24 -0500

    Made the modal component reusable.

--- src/app/common/modal-trigger.directive.ts
+++ src/app/common/modal-trigger.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, OnInit, Inject, ElementRef } from '@angular/core';
+import { Directive, OnInit, Inject, ElementRef, Input } from '@angular/core';
 import { JQ_TOKEN } from './jquery.service';
 // creates an instance of Jquery with JQ_Token injected
 @Directive({
   selector: '[modal-trigger]',
 })
+
+// the input paramater can also select the identifier for the input and then we can name it what we want
 export class ModalTriggerDirective implements OnInit {
   private el: HTMLElement;
+  @Input('modal-trigger') modalId:string
   constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $: any) {
     this.el = ref.nativeElement;
   }
   // opens up the modal with line 14
   ngOnInit() {
     this.el.addEventListener('click', (e) => {
-      this.$('#simple-modal').modal({});
+      this.$(`#${this.modalId}`).modal({});
     });
   }
 }
