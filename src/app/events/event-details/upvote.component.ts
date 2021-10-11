ebe93a47403fc70f56dad874891378cdd9951313
CyrusWarner <cyruswarner27@gmail.com>
2021-10-11 12:40:48 -0500

    Added a setter to voted input property to change color of vote icon based off of a boolean value.

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -1,29 +1,30 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'upvote',
   template: `
-  <div class="votingWidgetContainer pointable" (click)="onClick()">
-    <div class="well votingWidget">
-      <div class="votingButton">
-        <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-        <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
-      </div>
-      <div class="badge badge-inverse votingCount">
-        <div>{{count}}</div>
+    <div class="votingWidgetContainer pointable" (click)="onClick()">
+      <div class="well votingWidget">
+        <div class="votingButton">
+          <i [style.color]="iconColor" class="glyphicon glyphicon-heart"></i>
+        </div>
+        <div class="badge badge-inverse votingCount">
+          <div>{{ count }}</div>
+        </div>
       </div>
     </div>
-  </div>
   `,
-  styleUrls: ['./upvote.component.css']
-
+  styleUrls: ['./upvote.component.css'],
 })
 export class UpvoteComponent {
-  @Input() count:number;
-  @Input()voted:boolean;
+  @Input() count: number;
+  @Input() set voted(val:boolean) {
+    this.iconColor = val ? 'red' : 'white';
+  }
   @Output() vote = new EventEmitter();
+  iconColor: string;
 
-  onClick(){
+  onClick() {
     this.vote.emit({});
   }
 }
