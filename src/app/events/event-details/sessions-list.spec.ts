1804ad10128a0d5f8c648f8c1d138ced900c7e68
CyrusWarner <cyruswarner27@gmail.com>
2021-10-25 11:09:43 -0500

    Added a unit test for the sessions list ngOnChanges sort.

--- src/app/events/event-details/sessions-list.spec.ts
+++ src/app/events/event-details/sessions-list.spec.ts
@@ -29,5 +29,22 @@ describe('SessionListComponent', () => {
       expect(component.visibleSessions.length).toBe(2);
       expect(component.visibleSessions).not.toContain(<ISession>{ name: 'session 3', level: 'beginner' })
     });
+    it('should sort the sessions correctly', () => {
+      // Arrange
+      component.sessions = <ISession[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 3', level: 'intermediate' },
+        { name: 'session 2', level: 'beginner' },
+      ];
+      component.filterBy='all';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      //Act
+      component.ngOnChanges();
+
+      // Assert
+      expect(component.visibleSessions[2].name).toBe('session 3');
+    });
   });
 });
