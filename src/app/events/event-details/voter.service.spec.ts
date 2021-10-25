ee4db6ac3df73bc1112aa50d004833666a752a93
CyrusWarner <cyruswarner27@gmail.com>
2021-10-25 10:52:20 -0500

    Added unit test for delete voter Http delete.

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -8,22 +8,47 @@ describe('VoterService', () => {
   // Creates a new instance of the voterService before each test
   beforeEach(() => {
     // Creates a fake instance of the Http and we specifiy we only need it for a delete and post method
-    mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post'])
+    mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']);
     voterService = new VoterService(mockHttp);
   });
 
   describe('deleteVoter', () => {
     it('should remove a voter from a list of voters', () => {
       // Arrange
-      var session = {id: 6, voters: ["joe", "smith"]};
+      var session = { id: 6, voters: ['joe', 'smith'] };
       mockHttp.delete.and.returnValue(of(false));
 
       // Act
-      voterService.deleteVoter(3, <ISession>session, "joe");
+      voterService.deleteVoter(3, <ISession>session, 'joe');
 
       // Assert
       expect(session.voters.length).toBe(1);
-      expect(session.voters[0]).toBe("smith")
+      expect(session.voters[0]).toBe('smith');
+    });
+    it('should call http delete with the right url', () => {
+      // Arrange
+      var session = { id: 6, voters: ['joe', 'smith'] };
+      mockHttp.delete.and.returnValue(of(false));
+
+      // Act
+      voterService.deleteVoter(3, <ISession>session, 'joe');
+
+      // Assert
+      expect(mockHttp.delete).toHaveBeenCalledWith(
+        `/api/events/3/sessions/6/voters/joe`
+      );
+    });
+  });
+  describe('addVoter', () => {
+    it('should add a voter to the list of voters', () => {
+      // Arrange
+      var session = { id: 6, voters: ['joe', 'smith'] };
+      mockHttp.post.and.returnValue(of(false));
+      // Act
+      voterService.addVoter(3, <ISession>session, "bob")
+      // Assert
+      expect(session.voters.length).toBe(3);
+      expect(session.voters).toContain("bob")
     })
   })
 });
