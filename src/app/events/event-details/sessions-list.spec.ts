2bf339741e06b48db77a95f8239ea404658bc21b
CyrusWarner <cyruswarner27@gmail.com>
2021-10-25 11:07:37 -0500

    Added a unit test for the sessions list ngOnChanges filter.

---
+++ src/app/events/event-details/sessions-list.spec.ts
@@ -0,0 +1,33 @@
+import { SessionsListComponent } from './sessions-list.component';
+import { ISession } from '../shared/event.model';
+
+describe('SessionListComponent', () => {
+  let component: SessionsListComponent;
+  let mockAuthService;
+  let mockVoterService;
+
+  beforeEach(() => {
+    component = new SessionsListComponent(mockAuthService, mockVoterService);
+  });
+
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+      // Arrange
+      component.sessions = <ISession[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 2', level: 'intermediate' },
+        { name: 'session 3', level: 'beginner' },
+      ];
+      component.filterBy='intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      //Act
+      component.ngOnChanges();
+
+      // Assert
+      expect(component.visibleSessions.length).toBe(2);
+      expect(component.visibleSessions).not.toContain(<ISession>{ name: 'session 3', level: 'beginner' })
+    });
+  });
+});
